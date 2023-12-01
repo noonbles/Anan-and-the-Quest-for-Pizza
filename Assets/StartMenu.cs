e173bcc9c1e58e58d33b8ce967d42cf749c222ca
Adam Trac <atrac@umass.edu>
2023-11-30 19:36:28 -0500

    forgor 1

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject pizza;
+    public Button btn;
+
+    public void OnClick(){
+        SceneManager.LoadScene("Game_Level");
+        Debug.Log("WOOOOOT");
+    }
+
+    void Start(){
+        btn.onClick.AddListener(OnClick);
+    }
+
+    void Update()
+    {
+        pizza.transform.Rotate(Vector3.up * 7f * Time.deltaTime);
+    }
+}
