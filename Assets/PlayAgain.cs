13d772ef672f1d33f0c3d95d2b3e6838aa64f8d4
Adam Trac <atrac@umass.edu>
2023-12-14 12:15:33 -0500

    game over screen animations

--- Assets/PlayAgain.cs
+++ Assets/PlayAgain.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayAgain : MonoBehaviour
 {
+
+    public Button cont;
+    public Text gameOver;
+    void Start(){
+        LeanTween.scale(gameOver.gameObject, new Vector2(1, 1), 1.0f).setOnComplete(()=>{
+            Debug.Log("game over scaling done");
+            LeanTween.moveY(cont.gameObject, 400, 1.0f);
+        });
+    }
     public void OnClick(){
-        SceneManager.LoadScene("GameLevel");
-        //TODO: WRITE FILE SET GAME LEVEL TO 1;
+        DataWriter.writeData(1, false, 0);
+        SceneManager.LoadScene("GameLevel"); //might have to change later
     }
 }
