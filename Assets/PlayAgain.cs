a59567ed0958ef37fac5b25cc991f75c53a6d10d
Adam Trac <atrac@umass.edu>
2023-12-14 16:48:51 -0500

    SCORE IN GAME OVER

--- Assets/PlayAgain.cs
+++ Assets/PlayAgain.cs
@@ -9,10 +9,13 @@ public class PlayAgain : MonoBehaviour
 
     public Button cont;
     public Text gameOver;
+    public Text score;
     void Start(){
         LeanTween.scale(gameOver.gameObject, new Vector2(1, 1), 1.0f).setOnComplete(()=>{
             Debug.Log("game over scaling done");
             LeanTween.moveY(cont.gameObject, 400, 1.0f);
+            score.text = "Score: " + DataWriter.GetScore();
+            LeanTween.moveY(score.gameObject, 500, 1.0f);
         });
     }
     public void OnClick(){
