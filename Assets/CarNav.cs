ad9e2b491478cfd564cc32e396458edd0a2b0be5
Jeff L <jjl01332@gmail.com>
2023-12-14 00:19:36 -0500

    More cars, will alter the entire implementation tomorrow

--- Assets/CarNav.cs
+++ Assets/CarNav.cs
@@ -6,9 +6,11 @@ using UnityEngine.AI;
 public class CarNav : MonoBehaviour
 {
     public NavMeshAgent CarAgent;
+    public string Movement;
 
-    private int pos = 0;
-    private Vector3[] ToDriveDest = {
+    private int cpos = 0;
+    private int ccpos = 0;
+    private Vector3[] ClockwiseOuterDriveDest = {
         new Vector3(-389.85f, 69.66f, 14f),
         new Vector3(-397.85f, 69.66f, 46f),
         new Vector3(-329.85f, 69.66f, 50f),
@@ -18,10 +20,29 @@ public class CarNav : MonoBehaviour
         new Vector3(-209.85f, 69.66f, 18f),
         new Vector3(-313.85f, 69.66f, 14f),
     };
+    private Vector3[] CCOuterDriveDest = {
+        new Vector3(-205.85f, 69.66f, 22f),
+        new Vector3(-205.85f, 69.66f, 58f),
+        new Vector3(-285.85f, 69.66f, 54f),
+        new Vector3(-313.85f, 69.66f, 54f),
+        new Vector3(-389.85f, 69.66f, 54f),
+        new Vector3(-405.85f, 69.66f, 42f),
+        new Vector3(-405.85f, 69.66f, 18f),
+        new Vector3(-385.85f, 69.66f, 6f),
+        new Vector3(-313.85f, 69.66f, 6f),
+        new Vector3(-217.85f, 69.66f, 6f),
+    };
     // Start is called before the first frame update
     void Start()
     {
-        CarAgent.SetDestination(new Vector3(-389.85f, 69.66f, 14f));
+        if (Movement.Equals("C"))
+        {
+            CarAgent.SetDestination(ClockwiseOuterDriveDest[cpos]);
+        }
+        else if (Movement.Equals("CC"))
+        {
+            CarAgent.SetDestination(CCOuterDriveDest[ccpos]);
+        }
     }
 
     // Update is called once per frame
@@ -29,8 +50,17 @@ public class CarNav : MonoBehaviour
     {
         if(CarAgent.remainingDistance <= 0.05f)
         {
-            pos = (pos + 1) % 8;
-            CarAgent.SetDestination(ToDriveDest[pos]);
+            
+            if (Movement.Equals("C"))
+            {
+                cpos = (cpos + 1) % ClockwiseOuterDriveDest.Length;
+                CarAgent.SetDestination(ClockwiseOuterDriveDest[cpos]);
+            }
+            else if (Movement.Equals("CC"))
+            {
+                ccpos = (ccpos + 1) % (CCOuterDriveDest.Length);
+                CarAgent.SetDestination(CCOuterDriveDest[ccpos]);
+            }
         }
     }
 }
