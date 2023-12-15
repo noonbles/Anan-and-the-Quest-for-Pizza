051b442cdc7f0d3dcd5787398f9b33894daaba86
Adam Trac <atrac@umass.edu>
2023-12-15 16:06:26 -0500

    AUDIO FOR ALLLLLLL

--- Assets/House.cs
+++ Assets/House.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
+    public AudioClip source;
     private void OnTriggerEnter(Collider other)
     {
         Player p = other.gameObject.GetComponent<Player>();
         if (p && p.isHoldingPizza)
         {
+            AudioSource audioSource = p.GetComponent<AudioSource>();
+            audioSource.clip = source;
+            audioSource.Play();
             string selfName = gameObject.name;
             if(selfName.Equals(p.heldPizzaName))
                 p.correctDeliveries++;
